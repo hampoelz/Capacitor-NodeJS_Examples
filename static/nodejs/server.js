16e6f21b5fd1c74cdb9625426a7472b1917e63bf
hampoelz <rene_git@hampoelz.net>
2023-10-01 17:13:38 +0000

    chore: :wrench: Set up Node.js modules support

--- static/nodejs/server.js
+++ static/nodejs/server.js
@@ -1,9 +1,14 @@
 const { channel } = require('bridge');
+const { helloWorld } = require('hello-world-npm');
 
 // Listens to "msg-from-capacitor" from the Capacitor layer.
 channel.addListener('msg-from-capacitor', message => {
   console.log('[Node.js] Message from Capacitor: ' + message);
 
   // Sends a message back to the Capacitor layer.
-  channel.send('msg-from-nodejs', `Replying to the message "${message}".`, 'And optionally add more arguments.');
+  channel.send(
+    'msg-from-nodejs',
+    `Replying to the message "${message}".`,
+    `And optionally add more arguments: ${helloWorld()}`
+  );
 });
